720f18d8226508a826f3bc2676d5b389935d1c3d
大西貴斗 <oonishikito@oonishikitos-MacBook-Pro.local>
2025-07-20 16:46:02 +0900

    管理画面を追加

--- src/backend/meals/models.py
+++ src/backend/meals/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Meal(models.Model):
     name = models.CharField(max_length=100)
     calories = models.PositiveIntegerField()
-    date = models.DateField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"{self.name} - {self.calories} kcal"
+        return self.name
