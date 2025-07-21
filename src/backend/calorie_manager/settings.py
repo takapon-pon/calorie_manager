52e5eb0f31e22260bf46e6775abb4ec075aca834
大西貴斗 <oonishikito@oonishikitos-MacBook-Pro.local>
2025-07-21 14:00:15 +0900

    progreSQL追加

--- src/backend/calorie_manager/settings.py
+++ src/backend/calorie_manager/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'calorie_manager.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'calorie_db',
+        'USER': 'calorie_user',
+        'PASSWORD': 'calorie_pass',
+        'HOST': 'localhost',  # Dockerのポートバインド先
+        'PORT': '5432',
     }
 }
 
